025ab4af8bfa1f2f744d3d634a1306e45669ae59
interferon <myvictoryaremko@gmail.com>
2016-02-06 11:55:54 +0200

    commit

--- app.js
+++ app.js
@@ -11,6 +11,14 @@ app.get('/phones', function (req, res) {
   res.sendFile(__dirname + '/static_content/phones.json');
 });
 
+app.get('/cars', function (req, res) {
+  res.sendFile(__dirname + '/static_content/cars.json');
+});
+
+app.get('/boots', function (req, res) {
+  res.sendFile(__dirname + '/static_content/boots.json');
+});
+
 app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening ...');
 });
--- static_content/script.js
+++ static_content/script.js
@@ -1,7 +1,10 @@
 // **************************
 var xmlhttp = new XMLHttpRequest(),
     method = 'GET',
-    url = 'phones';
+    url1 = 'phones',
+    url2 = 'cars',
+    url3 = 'boots';
+
 
 
 xmlhttp.onreadystatechange = function () {
@@ -16,7 +19,17 @@ xmlhttp.onreadystatechange = function () {
 };
 
 
-document.getElementById("sendreq").addEventListener('click', function(){
-	xmlhttp.open(method, url, true);
+document.getElementById("sendreq1").addEventListener('click', function(){
+	xmlhttp.open(method, url1, true);
 	xmlhttp.send();
-})
+});
+
+document.getElementById("sendreq2").addEventListener('click', function(){
+	xmlhttp.open(method, url2, true);
+	xmlhttp.send();
+});
+
+document.getElementById("sendreq3").addEventListener('click', function(){
+	xmlhttp.open(method, url3, true);
+	xmlhttp.send();
+});
