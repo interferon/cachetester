acf2e84d262eec079c917f739ce871223a8f9b3f
interferon <myvictoryaremko@gmail.com>
2016-02-06 14:12:16 +0200

    commit

--- static_content/script.js
+++ static_content/script.js
@@ -8,8 +8,8 @@ var xmlhttp = new XMLHttpRequest(),
 
 
 xmlhttp.onreadystatechange = function () {
-    if (this.readyState == 4) {
-		if(this.status == 200) {
+    if (xmlhttp.readyState == 4) {
+		if(xmlhttp.status == 200) {
 			console.log(xmlhttp.responseText);
 		}
 	}
